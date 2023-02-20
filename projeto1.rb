2ae25f1679fae6347d0bd0ad9d5ea87546d1b7a0
Abner <abnerferreira84@gmail.com>
2023-02-20 11:10:26 -0300

    feat: Criando o seu Primeiro Projeto com Ruby

---
+++ projeto1.rb
@@ -0,0 +1,14 @@
+# Criando o seu Primeiro Projeto com Ruby
+
+print "Digite seu nome completo: "
+
+nome = gets.chomp
+
+print "Digite sua idade: "
+
+idade = gets.chomp.to_i
+
+puts "Registro de #{nome}, de #{idade} anos, realizado!"
+
+
+
